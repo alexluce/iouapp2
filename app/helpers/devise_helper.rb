2ad1fc48b6f664c25dc815dd17e35b4e5ab0c523
Alexander Luce <alexluce@gmail.com>
2013-08-01 19:06:05 -0400

    Fixed flash messages.

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -5,13 +5,23 @@ module DeviseHelper
       content_tag(:div, class: "alert alert-danger") do
         content_tag(:div, flash[flashed.to_sym])
       end
-    elsif resource.errors.any?
-      content_tag(:div, class: "alert alert-danger") do
-        resource.errors.each do |key, error|
-          content_tag(:div, resource.errors)
-        end
-      end
     end
   end
 
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+    html = <<-HTML
+    <div class="alert alert-danger">
+      <h4>#{sentence}</h4>
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
 end
